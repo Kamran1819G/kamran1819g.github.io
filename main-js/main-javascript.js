4159d7758870d9e290be5a8d5f58ef59f87c0b14
Kamran Khan <silentfinisher786@gmail.com>
2022-08-13 23:04:35 +0530

    update

---
+++ main-js/main-javascript.js
@@ -0,0 +1,7 @@
+// Cursor Script
+var cursor = document.getElementById("cursor");
+document.onmousemove = function(e){
+    cursor.style.left = (e.pageX - 25) + "px";
+    cursor.style.top = (e.pageY- 25) + "px";
+    cursor.style.display = "block";
+}
