fc755b8b67680a529b97616cbc58ee59545a9d9c
Kamran Khan <silentfinisher786@gmail.com>
2022-08-14 20:03:33 +0530

    update

--- main-js/main-javascript.js
+++ main-js/main-javascript.js
@@ -5,3 +5,21 @@ document.onmousemove = function(e){
     cursor.style.top = (e.pageY- 25) + "px";
     cursor.style.display = "block";
 }
+
+// loader Script
+setTimeout(function () {
+    $(".loader_bg").fadeToggle();
+}, 3900);
+
+// Text Rotation
+$(".simple-text-rotate").textrotator({
+    animation: "fade",
+    speed: 4000,
+});
+
+//Navbar
+$(window).on("scroll", function () {
+    $(window).scrollTop() >= 50
+  ? $(".sticky").addClass("stickyadd") : $(".sticky").removeClass("stickyadd");
+  });
+
